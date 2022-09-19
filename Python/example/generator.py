4460ce714c8d9f95e11283485c2fb0453efe8cf0
angrybirdpark <thesmileless90@gmail.com>
2022-09-19 18:08:21 +0900

    Add : Generator

--- Python/example/generator.py
+++ Python/example/generator.py
@@ -1,15 +1,25 @@
-from tkinter import N
-
-
 def get_natural_number():
     n = 0
     while True:
         n += 1
         yield n
 
+#Case 1
 print(get_natural_number())
+'''
+결과값 : <generator object get_natural_number at 0x102b43b30>
+'''
 
+#Case 2
 g = get_natural_number()
 
 for i in range(0, 100):
     print(next(g))
+'''
+결과값 :
+1
+2
+...
+99
+100
+'''
